d379f208f542b1568527015c4f1cfeace1446e84
Magic-cat-No765 <ndbghdvn@gmail.com>
2023-03-02 20:19:47 +0700

    fix tabs

--- web/main.py
+++ web/main.py
@@ -18,7 +18,7 @@ def index():
 if __name__ == '__main__':
     mc_port = 25565
     web_port = 80
-	subprocess.run(r"java -Xmx500M -Xms300M -jar /web/mc/server/minecraft_server.1.14.4.jar nogui", shell=True)
+    subprocess.run(r"java -Xmx500M -Xms300M -jar /web/mc/server/minecraft_server.1.14.4.jar nogui", shell=True)
     ngrok.set_auth_token(os.getenv('NGROK_AUTH'))
     app.ngrok_url = ngrok.connect(addr=mc_port, proto='tcp')
     print(app.ngrok_url)
