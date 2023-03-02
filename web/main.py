d25a007cda4dfa09f1345819115a2903fe4846b9
Magic-cat-No765 <ndbghdvn@gmail.com>
2023-03-02 19:55:39 +0700

    changed to 1 single Dockerfile

--- web/main.py
+++ web/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import HTMLResponse
 import uvicorn
 import nest_asyncio
 from pyngrok import ngrok
+import subprocess
 
 app = FastAPI()
 app.ngrok_url = ''
@@ -17,6 +18,7 @@ def index():
 if __name__ == '__main__':
     mc_port = 25565
     web_port = 80
+	subprocess.run(r"java -Xmx500M -Xms300M -jar minecraft_server.1.14.4.jar nogui", shell=True)
     ngrok.set_auth_token(os.getenv('NGROK_AUTH'))
     app.ngrok_url = ngrok.connect(addr=mc_port, proto='tcp')
     print(app.ngrok_url)
