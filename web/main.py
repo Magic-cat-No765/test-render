a2c6c26cc82571741ac5adfc1b1652853232d0f2
Magic-cat-No765 <ndbghdvn@gmail.com>
2023-03-02 20:44:34 +0700

    replaced subprocess with threading

--- web/main.py
+++ web/main.py
@@ -4,7 +4,8 @@ from fastapi.responses import HTMLResponse
 import uvicorn
 import nest_asyncio
 from pyngrok import ngrok
-import subprocess
+import os
+import threading
 
 app = FastAPI()
 app.ngrok_url = ''
@@ -14,11 +15,15 @@ app.ngrok_url = ''
 def index():
     return f"URL: {app.ngrok_url.public_url}"
 
+def mc_server():
+    os.system(r"java -Xmx500M -Xms300M -jar minecraft_server.1.14.4.jar nogui")
 
 if __name__ == '__main__':
     mc_port = 25565
     web_port = 80
-    subprocess.run(r"java -Xmx500M -Xms300M -jar minecraft_server.1.14.4.jar nogui", shell=True)
+    t = threading.Thread(target=mc_server)
+    t.daemon = True
+    t.start()
     ngrok.set_auth_token(os.getenv('NGROK_AUTH'))
     app.ngrok_url = ngrok.connect(addr=mc_port, proto='tcp')
     print(app.ngrok_url)
